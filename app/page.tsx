61be468e9890ad54b81dc912c3b05d63db99ddd2
dhruvalraval <iamdhruvalraval@gmail.com>
2025-07-05 19:49:22 +0530

    fonts added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <h1 className='text-4xl font-bold'>DSM</h1>
+    <div className='border-2 border-black'>
+      <h1 className='text-4xl font-bold'>DSM OKAY</h1>
+      <h2 className='text-2xl font-semibold font-space-grotesk'>THIS IS font-space-grotesk</h2>
     </div>
   )
 }
